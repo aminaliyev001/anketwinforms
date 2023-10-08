7936aeb5cbe3d363a310b247a23665f212bfc771
aminaliyev001 <72746607+aminaliyev001@users.noreply.github.com>
2023-10-08 17:32:36 +0400

    Add files via upload

---
+++ WinFormsApp1/Form2.Designer.cs
@@ -0,0 +1,376 @@
+﻿using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            progressBar1 = new ProgressBar();
+            textBox1 = new TextBox();
+            panel1 = new Panel();
+            panel2 = new Panel();
+            label10 = new Label();
+            textBox8 = new TextBox();
+            radioButton2 = new RadioButton();
+            radioButton1 = new RadioButton();
+            label9 = new Label();
+            dateTimePicker1 = new DateTimePicker();
+            label8 = new Label();
+            textBox7 = new TextBox();
+            textBox6 = new TextBox();
+            textBox5 = new TextBox();
+            label7 = new Label();
+            label6 = new Label();
+            label5 = new Label();
+            textBox4 = new TextBox();
+            textBox3 = new TextBox();
+            textBox2 = new TextBox();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            button2 = new Button();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Cursor = Cursors.Hand;
+            button1.Location = new Point(326, 5);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "Load";
+            button1.UseVisualStyleBackColor = true;
+            button1.UseWaitCursor = true;
+            button1.Click += button1_Click;
+            // 
+            // progressBar1
+            // 
+            progressBar1.Location = new Point(4, 35);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(397, 10);
+            progressBar1.TabIndex = 1;
+            progressBar1.Value = 20;
+            progressBar1.Visible = false;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(4, 6);
+            textBox1.Name = "textBox1";
+            textBox1.PlaceholderText = "enter name ...";
+            textBox1.Size = new Size(306, 23);
+            textBox1.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.AppWorkspace;
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(textBox1);
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(404, 33);
+            panel1.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            panel2.BorderStyle = BorderStyle.FixedSingle;
+            panel2.Controls.Add(label10);
+            panel2.Controls.Add(textBox8);
+            panel2.Controls.Add(radioButton2);
+            panel2.Controls.Add(radioButton1);
+            panel2.Controls.Add(label9);
+            panel2.Controls.Add(dateTimePicker1);
+            panel2.Controls.Add(label8);
+            panel2.Controls.Add(textBox7);
+            panel2.Controls.Add(textBox6);
+            panel2.Controls.Add(textBox5);
+            panel2.Controls.Add(label7);
+            panel2.Controls.Add(label6);
+            panel2.Controls.Add(label5);
+            panel2.Controls.Add(textBox4);
+            panel2.Controls.Add(textBox3);
+            panel2.Controls.Add(textBox2);
+            panel2.Controls.Add(label4);
+            panel2.Controls.Add(label3);
+            panel2.Controls.Add(label2);
+            panel2.Controls.Add(label1);
+            panel2.Location = new Point(12, 51);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(380, 422);
+            panel2.TabIndex = 4;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point);
+            label10.Location = new Point(32, 170);
+            label10.Name = "label10";
+            label10.Size = new Size(22, 20);
+            label10.TabIndex = 19;
+            label10.Text = "Id";
+            label10.Visible = false;
+            // 
+            // textBox8
+            // 
+            textBox8.Location = new Point(95, 167);
+            textBox8.Name = "textBox8";
+            textBox8.Size = new Size(251, 23);
+            textBox8.TabIndex = 18;
+            textBox8.Visible = false;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(289, 389);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(57, 19);
+            radioButton2.TabIndex = 17;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "Qadın";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(146, 389);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(43, 19);
+            radioButton1.TabIndex = 16;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Kişi";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point);
+            label9.Location = new Point(32, 387);
+            label9.Name = "label9";
+            label9.Size = new Size(36, 20);
+            label9.TabIndex = 15;
+            label9.Text = "Cins";
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(146, 346);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(200, 23);
+            dateTimePicker1.TabIndex = 14;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point);
+            label8.Location = new Point(31, 346);
+            label8.Name = "label8";
+            label8.Size = new Size(96, 20);
+            label8.TabIndex = 13;
+            label8.Text = "Doğum günü";
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(95, 275);
+            textBox7.Name = "textBox7";
+            textBox7.PlaceholderText = "+99451234567";
+            textBox7.Size = new Size(251, 23);
+            textBox7.TabIndex = 12;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(95, 243);
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(251, 23);
+            textBox6.TabIndex = 11;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(95, 208);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(251, 23);
+            textBox5.TabIndex = 10;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point);
+            label7.Location = new Point(31, 274);
+            label7.Name = "label7";
+            label7.Size = new Size(58, 20);
+            label7.TabIndex = 9;
+            label7.Text = "Telefon";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point);
+            label6.Location = new Point(31, 242);
+            label6.Name = "label6";
+            label6.Size = new Size(46, 20);
+            label6.TabIndex = 8;
+            label6.Text = "Şəhər";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point);
+            label5.Location = new Point(31, 211);
+            label5.Name = "label5";
+            label5.Size = new Size(39, 20);
+            label5.TabIndex = 7;
+            label5.Text = "Ölkə";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(95, 123);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(251, 23);
+            textBox4.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(95, 92);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(251, 23);
+            textBox3.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(95, 57);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(251, 23);
+            textBox2.TabIndex = 4;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.Location = new Point(31, 122);
+            label4.Name = "label4";
+            label4.Size = new Size(57, 20);
+            label4.TabIndex = 3;
+            label4.Text = "Ata adı";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(31, 91);
+            label3.Name = "label3";
+            label3.Size = new Size(50, 20);
+            label3.TabIndex = 2;
+            label3.Text = "Soyad";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(31, 60);
+            label2.Name = "label2";
+            label2.Size = new Size(28, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Ad";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Semibold", 13F, FontStyle.Italic, GraphicsUnit.Point);
+            label1.Location = new Point(160, 13);
+            label1.Name = "label1";
+            label1.Size = new Size(58, 25);
+            label1.TabIndex = 0;
+            label1.Text = "Anket";
+            // 
+            // button2
+            // 
+            button2.BackColor = SystemColors.Highlight;
+            button2.Cursor = Cursors.Hand;
+            button2.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point);
+            button2.ForeColor = SystemColors.ButtonHighlight;
+            button2.Location = new Point(44, 494);
+            button2.Name = "button2";
+            button2.Size = new Size(314, 31);
+            button2.TabIndex = 5;
+            button2.Text = "Save";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(404, 537);
+            Controls.Add(button2);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Controls.Add(progressBar1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "Form2";
+            Text = "Form1";
+            TopMost = true;
+            Load += Form2_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private Button button1;
+        private ProgressBar progressBar1;
+        private TextBox textBox1;
+        private Panel panel1;
+        private Panel panel2;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private Label label4;
+        private Label label8;
+        private TextBox textBox7;
+        private TextBox textBox6;
+        private TextBox textBox5;
+        private Label label7;
+        private Label label6;
+        private Label label5;
+        private RadioButton radioButton2;
+        private RadioButton radioButton1;
+        private Label label9;
+        private DateTimePicker dateTimePicker1;
+        private Button button2;
+        private Label label10;
+        private TextBox textBox8;
+    }
+}
---
+++ WinFormsApp1/Form2.cs
@@ -0,0 +1,216 @@
+﻿using Microsoft.VisualBasic.ApplicationServices;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Form2 : Form
+    {
+        private List<User> users = new List<User>();
+        private int progressValue = 0;
+        private System.Timers.Timer timer1 = new();
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        private void fillthefields(User user)
+        {
+            textBox2.Text = user.Name;
+            textBox3.Text = user.Surname;
+            textBox4.Text = user.Fname;
+            textBox5.Text = user.Country;
+            textBox6.Text = user.City;
+            textBox7.Text = user.Phone;
+            if (user.gender == 0)
+            {
+                radioButton1.Checked = true; radioButton2.Checked = false;
+            }
+            else { radioButton1.Checked = false; radioButton2.Checked = true; }
+            dateTimePicker1.Value = user.Birthday;
+            textBox8.Text = user.ID;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            progressBar1.Visible = true;
+            progressBar1.Value = 0;
+
+            progressValue = 0;
+            timer1.Interval = 10;
+            timer1.Elapsed += timer1_Tick;
+            timer1.AutoReset = true;
+            timer1.Start();
+
+            if (string.IsNullOrWhiteSpace(textBox1.Text) == true)
+                return;
+            string namesearch = textBox1.Text.ToLower();
+            foreach (var user in users)
+            {
+                if (user.Name.ToLower().Contains(namesearch))
+                {
+                    fillthefields(user);
+                    MessageBox.Show("User tapildi");
+                    return;
+                }
+            }
+            MessageBox.Show("User tapilmadi :(");
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            progressValue += 1;
+            if (progressValue <= 100)
+                progressBar1.Value = progressValue;
+            else
+            {
+                timer1.Stop();
+                progressBar1.Visible = false;
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string name = textBox2.Text;
+            string surname = textBox3.Text;
+            string fname = textBox4.Text;
+            string country = textBox5.Text;
+            string city = textBox6.Text;
+            string phone = textBox7.Text;
+            DateTime birthday = dateTimePicker1.Value;
+            Gender_enum gender = default;
+            string getid = textBox8.Text;
+            if (radioButton1.Checked == true)
+                gender = Gender_enum.Male;
+            else gender = Gender_enum.Female;
+
+            if (string.IsNullOrEmpty(textBox8.Text) == false)
+            {
+                foreach (var user in users)
+                {
+                    if (user.ID == getid)
+                    {
+                        try
+                        {
+                            user.Name = name;
+                            user.Surname = surname;
+                            user.Fname = fname;
+                            user.Country = country;
+                            user.City = city;
+                            user.Phone = phone;
+                            user.Birthday = birthday;
+                            user.gender = gender;
+                            savetofile();
+                            textBox2.Text = string.Empty;
+                            textBox3.Text = string.Empty;
+                            textBox4.Text = string.Empty;
+                            textBox5.Text = string.Empty;
+                            textBox6.Text = string.Empty;
+                            textBox7.Text = string.Empty;
+                            dateTimePicker1.Text = string.Empty;
+                            textBox8.Text = string.Empty;
+                            MessageBox.Show("Melumatlar yenilendi :)");
+                            return;
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show(ex.Message);
+                            return;
+                        }
+                    }
+                }
+                return;
+            }
+            string id = Random.Shared.Next(0, 9999999).ToString();
+            try
+            {
+                User user = new(name, surname, fname, country, city, phone, birthday, gender, id);
+                users.Add(user);
+                savetofile();
+                textBox2.Text = string.Empty;
+                textBox3.Text = string.Empty;
+                textBox4.Text = string.Empty;
+                textBox5.Text = string.Empty;
+                textBox6.Text = string.Empty;
+                textBox7.Text = string.Empty;
+                dateTimePicker1.Text = string.Empty;
+                MessageBox.Show("Elave edildi :)");
+            }
+            catch (Exception ee)
+            {
+                MessageBox.Show(ee.Message);
+            }
+        }
+        private void savetofile()
+        {
+            string fileName = "users.json";
+            string jsonString = JsonSerializer.Serialize(users);
+            File.WriteAllText(fileName, jsonString);
+        }
+        private void loadfromfile()
+        {
+            string fileName = "users.json";
+            if (File.Exists(fileName) == false)
+                return;
+            string jsonString = File.ReadAllText(fileName);
+            users = JsonSerializer.Deserialize<List<User>>(jsonString);
+        }
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            loadfromfile();
+        }
+    }
+    public enum Gender_enum { Male, Female }
+    public class User
+    {
+        public static bool RegexPhone(string phone)
+        {
+            string pattern = @"^\+994(50|51|70|77|55|10|90)\d{7}$";
+            return Regex.IsMatch(phone, pattern);
+        }
+        public string ID { get; set; }
+        private string name;
+        public string Name { get => name; set { if (!string.IsNullOrWhiteSpace(value)) name = value; else throw new Exception("Ad bos qala bilmez"); } }
+        private string surname;
+        public string Surname { get => surname; set { if (!string.IsNullOrWhiteSpace(value)) surname = value; else throw new Exception("Soyad bos qala bilmez"); } }
+        private string fname;
+        public string Fname { get => fname; set { if (!string.IsNullOrWhiteSpace(value)) fname = value; else throw new Exception("Ata adi bos qala bilmez"); } }
+        private string country;
+        public string Country { get => country; set { if (!string.IsNullOrWhiteSpace(value)) country = value; else throw new Exception("Olke adi bos qala bilmez"); } }
+        private string city;
+        public string City { get => city; set { if (!string.IsNullOrWhiteSpace(value)) city = value; else throw new Exception("Seher adi bos qala bilmez"); } }
+        private string phone;
+        public string Phone { get => phone; set { if (RegexPhone(value)) phone = value; else throw new Exception("Telefon nomresi duzgun formatda verilmeyib"); } }
+        private DateTime birthday;
+        public DateTime Birthday
+        {
+            get => birthday; set
+            {
+                TimeSpan age = DateTime.Today - value;
+                if (age.TotalDays >= 18 * 365)
+                    birthday = value;
+                else
+                    throw new Exception("18 yasdan kicik vetendaslar bu anketi doldura bilmezler");
+            }
+        }
+        public Gender_enum gender { get; set; }
+        public User() { }
+        public User(string name, string surname, string fname, string country, string city, string phone, DateTime birthday, Gender_enum gender, string id)
+        {
+            Name = name;
+            Surname = surname;
+            Fname = fname;
+            Country = country;
+            City = city;
+            Phone = phone;
+            Birthday = birthday;
+            this.gender = gender;
+            ID = id;
+        }
+    }
+}
